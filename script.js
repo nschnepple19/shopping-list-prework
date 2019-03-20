2e8503045f089360f60dfc802e3de8c01790b82c
NickSchnepple <nschnepple@cnm.edu>
2019-03-20 17:39:26 -0600

    added grave accent instead of single quote.

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ function addItemToShoppingList() {
 	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
 	console.log("Item HTML: ", itemHtml);
 	let itemListRef = document.getElementById("shopping-list");
-	itemListRef.insertAdjacentHTML("afterend", itemHtml)
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
 }
 
 function createListItemHtml(itemName, itemAmount) {
-	return '<li>
+	return `	<li>
 					${itemName} - ${itemAmount}
-		</li>';
+			 </li>`;
 }
